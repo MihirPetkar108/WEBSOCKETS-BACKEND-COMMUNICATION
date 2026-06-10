10b1ff5b11d8027d50e8fb0a289904193acebf62
MihirPetkar108 <mihirpetkar2006@gmail.com>
2026-06-11 00:05:20 +0530

    Relay Server Logic

---
+++ backend_js/relay_server/index.ts
@@ -0,0 +1,24 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const port = 3000;
+
+const relayServer = new WebSocketServer({ port });
+console.log(`Connected to the Relay Server on port ${port}`);
+
+const servers: WebSocket[] = [];
+
+relayServer.on("connection", function connection(ws) {
+    ws.on("error", console.error);
+
+    servers.push(ws);
+
+    ws.on("message", function message(data) {
+        const { type } = JSON.parse(data.toString());
+
+        if (type === "chat") {
+            servers.forEach((socket) => {
+                socket.send(data);
+            });
+        }
+    });
+});
