a1d874ef656753fdcc170badb2c35d6792d94f71
MihirPetkar108 <mihirpetkar2006@gmail.com>
2026-06-11 11:58:20 +0530

    Made it compatible to send data to python files and vice versa

--- backend_js/relay_server/index.ts
+++ backend_js/relay_server/index.ts
@@ -8,17 +8,17 @@ console.log(`Connected to the Relay Server on port ${port}`);
 const servers: WebSocket[] = [];
 
 relayServer.on("connection", function connection(ws) {
-    ws.on("error", console.error);
+  ws.on("error", console.error);
 
-    servers.push(ws);
+  servers.push(ws);
 
-    ws.on("message", function message(data) {
-        const { type } = JSON.parse(data.toString());
+  ws.on("message", function message(data) {
+    const { type } = JSON.parse(data.toString());
 
-        if (type === "chat") {
-            servers.forEach((socket) => {
-                socket.send(data);
-            });
-        }
-    });
+    if (type === "chat") {
+      servers.forEach((socket) => {
+        socket.send(data);
+      });
+    }
+  });
 });
--- backend_js/websocket_server/index.ts
+++ backend_js/websocket_server/index.ts
@@ -1,106 +1,132 @@
 import WebSocket, { WebSocketServer } from "ws";
 
-const port = 8081;
+const port = 8080;
+
+const relayPort = 3000;
+// For js relay server
+const relaySocket = new WebSocket(`ws://localhost:${relayPort}/rs`);
+
+// For python relay server
+// const relaySocket = new WebSocket(`ws://127.0.0.1:3000/rs`);
 
 const wss = new WebSocketServer({ port });
 console.log(`Connected to WebSocket Server on port:- ${port}`);
 
-const relayPort = 3000;
-const relaySocket = new WebSocket(`ws://localhost:${relayPort}`);
-
 interface message {
-    name: string;
-    message: string;
+  name: string;
+  message: string;
 }
 interface Room {
-    users: WebSocket[];
-    messages: message[];
+  users: WebSocket[];
+  messages: message[];
 }
 
 const Rooms: Record<string, Room> = {
-    // roomId: {
-    //     users: [ws1, ws2, ws3],
-    //     content: [
-    //          {
-    //              "name": "Mihir",
-    //              "message": "hi"
-    //          },
-    //          {
-    //              "name": "Soham",
-    //              "message": "hello"
-    //          },
-    //
-    //      ]
-    // }
+  // roomId: {
+  //     users: [ws1, ws2, ws3],
+  //     content: [
+  //          {
+  //              "name": "Mihir",
+  //              "message": "hi"
+  //          },
+  //          {
+  //              "name": "Soham",
+  //              "message": "hello"
+  //          },
+  //
+  //      ]
+  // }
 };
 
 relaySocket.onopen = function open() {
-    console.log(`Connected to Relay Server on PORT ${relayPort}`);
+  console.log(`Connected to Relay Server on PORT ${relayPort}`);
 };
 
 relaySocket.onerror = function error(error) {
-    console.log(`Relay Websocket error:- ${error.message}`);
+  console.log(`Relay Websocket error:- ${error.message}`);
 };
 
-relaySocket.onmessage = function message({data}) {
-    const { type, roomId, name, content } = JSON.parse(data.toString());
+relaySocket.onmessage = function message({ data }) {
+  const { type, roomId, name, content, host, port } = JSON.parse(
+    data.toString(),
+  );
 
-    if (type === "chat") {
-        if (!Rooms[roomId]) {
-            console.log(`No room with roomId ${roomId} found!`);
-            return;
-        }
+  if (type === "connected") {
+    console.log(`Connected to relay ${host}:${port}`);
+    return;
+  }
 
-        const room = Rooms[roomId];
+  if (type === "join") {
+    if (!Rooms[roomId]) {
+      Rooms[roomId] = {
+        users: [],
+        messages: [],
+      };
 
-        room.messages.push({ name, message: content });
-
-        room.users.map((socket) => {
-            socket.send(
-                `Message received from ${name} in roomId:- ${roomId} with content:- ${content}`,
-            );
-        });
+      console.log(`Created room ${roomId} from relay`);
     }
+
+    return;
+  }
+
+  if (type === "chat") {
+    if (!Rooms[roomId]) {
+      Rooms[roomId] = {
+        users: [],
+        messages: [],
+      };
+
+      console.log(`Created room ${roomId} from chat relay`);
+    }
+
+    const room = Rooms[roomId];
+
+    room.messages.push({ name, message: content });
+
+    room.users.map((socket) => {
+      socket.send(
+        `Message received from ${name} in roomId:- ${roomId} with content:- ${content}`,
+      );
+    });
+  }
 };
 
 wss.on("connection", function connection(ws) {
-    ws.on("error", console.error);
+  ws.on("error", console.error);
 
-    ws.on("message", function messsage(data) {
-        const { type, roomId } = JSON.parse(data.toString());
+  ws.on("message", function messsage(data) {
+    const { type, roomId } = JSON.parse(data.toString());
 
-        if (type === "join") {
-            if (!Rooms[roomId]) {
-                Rooms[roomId] = {
-                    users: [],
-                    messages: [],
-                };
-            }
+    if (type === "join") {
+      if (!Rooms[roomId]) {
+        Rooms[roomId] = {
+          users: [],
+          messages: [],
+        };
+      }
 
-            relaySocket.send(data);
+      relaySocket.send(data.toString());
 
-            Rooms[roomId].users.push(ws);
+      Rooms[roomId].users.push(ws);
 
-            ws.send(`User connected to room ${roomId}`);
-        }
+      ws.send(`User connected to room ${roomId}`);
+    }
 
-        if (type === "chat") {
-            relaySocket.send(data);
-        }
+    if (type === "chat") {
+      relaySocket.send(data.toString());
+    }
 
-        if (type === "getChat") {
-            const room = Rooms[roomId];
-            if (!room) {
-                console.log(`No room with roomId ${roomId} found!`);
-                return;
-            }
+    if (type === "getChat") {
+      const room = Rooms[roomId];
+      if (!room) {
+        console.log(`No room with roomId ${roomId} found!`);
+        return;
+      }
 
-            const message = room.messages;
-            console.log("GET CHAT MESSAGE:- ", message)
-            ws.send(JSON.stringify(message));
-        }
-    });
+      const message = room.messages;
+      ws.send(JSON.stringify(message));
+    }
+  });
 
-    ws.send(`User connected to the WebSocket Server on port ${port}`);
+  ws.send(`User connected to the WebSocket Server on port ${port}`);
 });
-
